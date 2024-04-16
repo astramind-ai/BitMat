90287373f30ac9b906f4308bd68d6375557bf82a
Marco <121761685+mlinmg@users.noreply.github.com>
2024-04-16 16:50:47 +0200

    fixed __init__ to add AutoClass

--- bitmat/__init__.py
+++ bitmat/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['BitLinear', 'convert_hf_model', 'pack_ternary', 'unpack_ternary', 'Gemma158ForCausalLM', 'Mistral158ForCausalLM', 'Llama158ForCausalLM']
+__all__ = ['BitLinear', 'convert_hf_model', 'pack_ternary', 'unpack_ternary', 'Auto158ModelForCausalLM', 'Gemma158ForCausalLM', 'Mistral158ForCausalLM', 'Llama158ForCausalLM']
 from .utils.convert_hf_model import convert_hf_model
 from .utils.packing import pack_ternary, unpack_ternary
 from .bitlinear import BitLinear
-from .utils.model_hijacks.gemma_1_58b import Gemma158ForCausalLM
-from .utils.model_hijacks.mistral_1_58b import Mistral158ForCausalLM
-from .utils.model_hijacks.llama_1_58b import Llama158ForCausalLM
+from .utils.modeling.model_hijacks.gemma_1_58b import Gemma158ForCausalLM
+from .utils.modeling.model_hijacks.mistral_1_58b import Mistral158ForCausalLM
+from .utils.modeling.model_hijacks.llama_1_58b import Llama158ForCausalLM
+from .utils.modeling import Auto158ModelForCausalLM
